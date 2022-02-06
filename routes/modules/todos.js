dd4187e2badb2c4650d5bf678cf1fbf0a154b058
shuyuhuang23 <susan50337@gmail.com>
2022-02-06 18:30:30 +0800

    add edit feature

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -14,4 +14,27 @@ router.get('/:id', (req, res) => {
         .catch(error => { console.log(error) })
 })
 
+router.get('/:id/edit', (req, res) => {
+    const userId = req.user.id
+    const id = req.params.id
+
+    return Todo.findOne({ where: { id: id, UserId: userId } })
+        .then(todo => {
+            todo = todo.toJSON()
+            res.render('edit', { todo })
+        })
+        .catch(error => console.log(error))
+})
+
+
+router.put('/:id', (req, res) => {
+    const userId = req.user.id
+    const id = req.params.id
+    const { name, isDone } = req.body
+
+    Todo.update({ name: name, isDone: isDone === 'on' }, { where: { id: id, UserId: userId } })
+        .then(() => res.redirect(`/todos/${id}`))
+        .catch(error => console.log(error))
+})
+
 module.exports = router
