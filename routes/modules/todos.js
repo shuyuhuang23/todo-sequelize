aacf6506fa9c0da6f8f2a96d3b6b900be46fd1c8
shuyuhuang23 <susan50337@gmail.com>
2022-02-06 20:52:45 +0800

    add create feature

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -4,12 +4,24 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 
+router.get('/new', (req, res) => {
+    res.render('new')
+})
+
+router.post('/', (req, res) => {
+    const userId = req.user.id
+    const name = req.body.name
+    return Todo.create({ name: name, UserId: userId })
+        .then(() => res.redirect('/'))
+        .catch(error => console.log(error))
+
+})
+
 router.get('/:id', (req, res) => {
     const userId = req.user.id
     const id = req.params.id
     return Todo.findOne({ where: { id: id, UserId: userId } })
         .then(todo => {
-            console.log(todo.toJSON())
             res.render('detail', { todo: todo.toJSON() })
         })
         .catch(error => { console.log(error) })
